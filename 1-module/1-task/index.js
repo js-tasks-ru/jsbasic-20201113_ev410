8b9205c16a73391273d16b98c408d77ee104a586
Elena Skorobogatko <ev410@mail.ru>
2020-11-22 23:37:12 +0400

    task2. Факториал числа

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,21 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+	let  factorial = 1;
+
+	if (n != 0){
+	  	do {
+	  		factorial = factorial * n;
+	  		n--;
+	  	} while (n > 1);
+  	}
+
+  	return factorial;
 }
+
+let num = +prompt('Введите число','')
+
+if (Object.is(num,NaN) != true && num >= 0 && (num ^ 0) === num)
+	alert(`Факториал числа: ${factorial(num)}`)
+else
+	alert(`Задано не натуральное число`);
