1dfed752b20ef68743858f360297d0fa631679de
Elena Skorobogatko <ev410@mail.ru>
2020-11-24 13:22:31 +0400

    task5. Проверка на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -3,6 +3,19 @@
  * @param {string} str base
  * @returns {boolean}
  */
+
 function checkSpam(str) {
-  // ваш код...
+	
+	let spamValues = ['1xBet','XXX'],
+		spamCnt = 0;
+
+	str = str.toUpperCase();
+	
+	for (let i = 0; i < spamValues.length; i++)
+		spamCnt += (str.includes(spamValues[i].toUpperCase()) ? 1 : 0)
+
+  	if (spamCnt > 0)
+  		return true
+  	else
+  		return false;
 }
