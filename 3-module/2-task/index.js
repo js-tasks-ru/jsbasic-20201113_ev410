1a00699054e960e982c72354ede141bd3636e5ad
Elena Skorobogatko <ev410@mail.ru>
2021-01-02 03:37:25 +0400

    task3.2. Фильтрация по диапазону

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -6,4 +6,13 @@
  */
 function filterRange(arr, a, b) {
   // ваш код...
+	let filterArr = arr.map(item => (item >= a && item <=b) ? item : null);
+	let pos = filterArr.indexOf(null);
+
+	while (pos !== -1) {
+		filterArr.splice(pos, 1);
+	  	pos = filterArr.indexOf(null);	
+	}
+
+  return filterArr
 }
